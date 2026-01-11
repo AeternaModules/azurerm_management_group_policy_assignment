2760facbd313152b6ba43b74a31bd7c9d758edc7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:46:43 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "management_group_policy_assignments" {
+  description = "All management_group_policy_assignment resources"
+  value       = azurerm_management_group_policy_assignment.management_group_policy_assignments
+}
+
---
+++ variables.tf
@@ -0,0 +1,73 @@
+variable "management_group_policy_assignments" {
+  description = <<EOT
+Map of management_group_policy_assignments, attributes below
+Required:
+    - management_group_id
+    - name
+    - policy_definition_id
+Optional:
+    - description
+    - display_name
+    - enforce
+    - location
+    - metadata
+    - not_scopes
+    - parameters
+    - identity (block):
+        - identity_ids (optional)
+        - type (required)
+    - non_compliance_message (block):
+        - content (required)
+        - policy_definition_reference_id (optional)
+    - overrides (block):
+        - selectors (optional, block):
+            - in (optional)
+            - kind (optional)
+            - not_in (optional)
+        - value (required)
+    - resource_selectors (block):
+        - name (optional)
+        - selectors (required, block):
+            - in (optional)
+            - kind (required)
+            - not_in (optional)
+EOT
+
+  type = map(object({
+    management_group_id  = string
+    name                 = string
+    policy_definition_id = string
+    description          = optional(string)
+    display_name         = optional(string)
+    enforce              = optional(bool, true)
+    location             = optional(string)
+    metadata             = optional(string)
+    not_scopes           = optional(list(string))
+    parameters           = optional(string)
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    non_compliance_message = optional(object({
+      content                        = string
+      policy_definition_reference_id = optional(string)
+    }))
+    overrides = optional(object({
+      selectors = optional(object({
+        in     = optional(list(string))
+        kind   = optional(string, "policyDefinitionReferenceId")
+        not_in = optional(list(string))
+      }))
+      value = string
+    }))
+    resource_selectors = optional(object({
+      name = optional(string)
+      selectors = object({
+        in     = optional(list(string))
+        kind   = string
+        not_in = optional(list(string))
+      })
+    }))
+  }))
+}
+
